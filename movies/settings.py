30a2cb26b313d42cb73ff911dfb47b06cf4767cb
Todd Trost <68034370+ToddTrost@users.noreply.github.com>
2024-02-24 20:50:57 -0600

    Update settings.py
    Update for PythonAnywhere hosting

--- movies/settings.py
+++ movies/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0vi1=!4752(f=0d+yyl(x8_2*=e=m7e^64&byp_$2n!ft%@&#e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
